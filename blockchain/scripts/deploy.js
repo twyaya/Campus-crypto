f9878de26a4fcf11b6c8e0066955f3f14ce182c3
twyaya <otis8496@gmail.com>
2025-06-20 15:30:44 +0800

    Update deploy.js

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,9 +1,12 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const initialSupply = 100000;
+  // 部署 MyToken 合約
+  // ethers.getContractFactory() 會回傳一個合約工廠，這個工廠可以用來部署合約 
   const Token = await ethers.getContractFactory("MyToken");
-  const token = await Token.deploy(); // 這邊 deploy() 會送出交易
-  await token.deployed();             // ✅ 等待部署完成（這是正確的）
+  const token = await Token.deploy(initialSupply); // 這邊 deploy() 會送出交易
+  await token.deployed();             // 等待部署完成
 
   console.log("Token deployed to:", token.address);
 }
